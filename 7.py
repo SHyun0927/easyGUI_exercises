c4219383707e8708609b4717774eeb7acc07d274
SHyun0352 <haj3@middleton.school.nz>
2023-03-13 09:35:14 +1300

    Update 7.py

--- 7.py
+++ 7.py
@@ -3,24 +3,31 @@ import easygui
 
 
 def spelling_convertor():
-    nz_word = easygui.enterbox("Please write word in NZ spelling", "NZ word to US word converter")
+    while True:
+        nz_word = easygui.enterbox("Please write word in NZ spelling", "NZ word to US word converter")
 
-    if nz_word == "":
-        easygui.msgbox("Please enter a word to convert", "Error!")
-        return
-
-    if "our" in nz_word:
-        us_word = nz_word.replace("our", "or")
-
-    else:
-        if "ise" in nz_word or "yse" in nz_word:
-            us_word = nz_word.replace("ise", "ize").replace("yse", "yze")
-
-        else:
-            easygui.msgbox("There are no nz word detected.")
+        if nz_word == "":
+            easygui.msgbox("Please enter a word to convert", "Error!")
             return
 
-    easygui.msgbox(f"The American spelling for {nz_word} is {us_word}")
+        if "our" in nz_word:
+            us_word = nz_word.replace("our", "or")
+
+        else:
+            if "ise" in nz_word or "yse" in nz_word:
+                us_word = nz_word.replace("ise", "ize").replace("yse", "yze")
+
+            else:
+                us_word = "--"
+                easygui.msgbox("There are no nz word detected.")
+
+        easygui.buttonbox(f"The American spelling for {nz_word} is {us_word}. \n"
+                          f"Do you want to convert another word?", choices=["Yes", "No"])
+        restart = easygui.buttonbox()
+
+        if restart == "No":
+            break
 
 
 spelling_convertor()
+easygui.msgbox("Thank you for using the convertor!")
