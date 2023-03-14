5cfcb0f7b6690de60386e2ef11f120d44732f375
SHyun0352 <haj3@middleton.school.nz>
2023-03-14 19:35:26 +1300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,56 @@
+import easygui
+import random
+
+
+# 주사위 5개를 숫자 1-6사이로 굴린다
+def dice_roll():
+    dice = [random.randint(1, 6) for i in range(5)]
+    return dice
+
+
+# 결과를 감지한다.
+def detect_result(dice):
+    # 5가지가 모두 같이 나온다면 야추
+    if dice.count(dice[0]) == 5:
+        return "Yahtzee!"
+
+    # 4가지가 같이 나온다면 Four  of a kind
+    # 해당 코드가 작동하는 방식은 list에서 0번(첫번쨰)칸 혹은 1번(두번쨰) 칸의 숫자를 분석하며 해당 숫자들과 같은 숫자가 있는지 찾는다.
+    elif dice.count(dice[0]) == 4 or dice.count(dice[1]) == 4:
+        return "Four of a kind"
+
+    # 3가지가 같다면 나온다면 Three of kind을 출력.
+    # 여기서도 1번, 2번, 혹은 3번칸을 체크하여 같은 숫자가 있는지 본다.
+    elif dice.count(dice[0]) == 3 or dice.count(dice[1]) == 3 or dice.count(dice[2]) == 3:
+        return "Three of a kind"
+
+    # 만약 같은 숫자가 2개 혹은 존재하지 않는다면 이 문구를 표시한다.
+    else:
+        return "Better luck next time..."
+
+
+# 코드를 전부 잇는 메인루틴 function
+def main():
+    dice = dice_roll()
+
+    # 이 부분은 유저가 주사위를 3번 다시 굴릴 수 있게 해 준다.
+    for i in range(3):
+        # 여기는 버튼박스를 활용하여 다시 굴릴지, 선택지에 그대로 있을지 선택한다.
+        again = easygui.buttonbox(f"You rolled: {dice} \n"
+                                  f"Choose: ", "Rolling...", choices=["Reroll", "Stick"])
+
+        # 만약 다시 한다면, dice_roll() function을 가져와 다시 굴린다.
+        if again == "Reroll":
+            dice = dice_roll()
+
+        # 만약 원하지 않는다면 해당 loop를 break한다.
+        else:
+            break
+
+    # 결과 (선택한 주사위 및 결과(야추, Four/Three of a kind)를 출력)
+    easygui.msgbox(f"Your final roll is: {dice} \n"
+                   f"{detect_result(dice)}", "Result!")
+
+
+# 메인 루틴
+main()
