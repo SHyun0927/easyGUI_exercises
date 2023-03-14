79c664787eb94bb7177650337cdf7e0b9f6de648
SHyun0352 <haj3@middleton.school.nz>
2023-03-15 08:59:02 +1300

    Update 10.py

--- 10.py
+++ 10.py
@@ -33,25 +33,38 @@ def detect_result(dice):
 
 # 코드를 전부 잇는 메인루틴 function
 def main():
-    dice = dice_roll()
+    # while True loop를 사용하여 유저가 다시 플레이하길 원할 떄 무한적으로 반복할 수 있게 한다
+    while True:
+        # 주사위를 굴린다
 
-    # 이 부분은 유저가 주사위를 3번 다시 굴릴 수 있게 해 준다.
-    for i in range(3):
-        # 여기는 버튼박스를 활용하여 다시 굴릴지, 선택지에 그대로 있을지 선택한다.
-        again = easygui.buttonbox(f"You rolled: {dice} \n"
-                                  f"Choose: ", "Rolling...", choices=["Reroll", "Stick"])
+        dice = dice_roll()
+        # 이 부분은 유저가 주사위를 3번 다시 굴릴 수 있게 해 준다.
+        for i in range(2):
+            # 여기는 버튼박스를 활용하여 다시 굴릴지, 선택지에 그대로 있을지 선택한다.
+            again = easygui.buttonbox(f"You rolled: {dice} \n"
+                                      f"Choose: ", "Rolling...", choices=["Reroll", "Stick"])
 
-        # 만약 다시 한다면, dice_roll() function을 가져와 다시 굴린다.
-        if again == "Reroll":
-            dice = dice_roll()
+            # 만약 다시 한다면, dice_roll() function을 가져와 다시 굴린다.
+            if again == "Reroll":
+                dice = dice_roll()
 
-        # 만약 원하지 않는다면 해당 loop를 break한다.
-        else:
+            # 만약 원하지 않는다면 해당 loop를 break한다.
+            else:
+                break
+
+        # 결과 (선택한 주사위 및 결과(야추, Four/Three of a kind)를 출력)
+        easygui.msgbox(f"Your final roll is: {dice} \n"
+                       f"{detect_result(dice)}", "Result!")\
+
+        # 유저가 3개의 턴을 전부 행하면, 다시 플레이하고 싶은지 물어본다.
+        restart = easygui.buttonbox("You've had 3 turns. Do you want to play again?", "Try?", choices=["Yes", "No"])
+
+        # 만약 아니라고 대답한다면, 종료 인사를 표시한 뒤 while loop을 break한다.
+        if restart == "No":
+            easygui.msgbox("Thank you for playing Yahtzee lite!", "Thank you!")
             break
 
-    # 결과 (선택한 주사위 및 결과(야추, Four/Three of a kind)를 출력)
-    easygui.msgbox(f"Your final roll is: {dice} \n"
-                   f"{detect_result(dice)}", "Result!")
+
 
 
 # 메인 루틴
